987c9d59551a465a4779b6362c0ca717d9095dc6
Ronny Valdivieso <ron_251@hotmail.com>
2017-03-03 15:50:11 -0500

    cálculo de depreciacion

--- activofijo/settings.py
+++ activofijo/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.activo',
-    'apps.usuario'
+    'apps.usuario',
+    'apps.depreciacion'
 )
 
 MIDDLEWARE_CLASSES = (
--- apps/depreciacion/views.py
+++ apps/depreciacion/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from activofijo.models import Depreciacion
+from activofijo.models import Depreciacion, Activo
+import datetime
+from django.utils.timesince import timesince
 
 # Create your views here.
 def listar(request):
 	depreciacion = Depreciacion.objects.all()
+
+	for dep in depreciacion:
+		activo = Activo.objects.get(id=dep.activo.id)
+		dep.valor_depreciado = float(activo.costo / activo.vida_util)
+		dep.fecha_inicio = activo.fecha_compra
+		fecha_inicio = dep.fecha_inicio
+		dep.fecha_fin = (fecha_inicio + datetime.timedelta(activo.vida_util*365)).isoformat()
+		dep.fecha_fin = datetime.datetime.strptime(dep.fecha_fin, "%Y-%m-%d").date()
+		dep.depreciacion_acumulada = time_until(dep.fecha_inicio.year) * round(float(dep.valor_depreciado), 2)
+		dep.save()
+	
 	contexto = {'depreciaciones':depreciacion}
 
 	return render(request, 'depreciacion/show.html', contexto)
+
+def time_until(value):
+    now = datetime.datetime.now().year
+    try:
+        difference = now - value
+        print difference
+    except:
+        return value
+
+    return difference
